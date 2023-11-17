aa31b8194d4395503976a6bf9fb224e26041962b
ssh1141 <sashsoler7@gmail.com>
2023-11-17 10:56:04 -0300

    cambie el nombre de la clase button

--- js/funciones.js
+++ js/funciones.js
@@ -17,7 +17,7 @@ function crearCardHTML(producto) {
                 <h6>${producto.precio}</h6>
             </div>
             <div class="button">
-              <button class="boton-añadir" id="${producto.id}"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag-heart" viewBox="0 0 16 16">
+              <button class="boton-add" id="${producto.id}"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag-heart" viewBox="0 0 16 16">
               <path fill-rule="evenodd" d="M10.5 3.5a2.5 2.5 0 0 0-5 0V4h5v-.5Zm1 0V4H15v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4h3.5v-.5a3.5 3.5 0 1 1 7 0ZM14 14V5H2v9a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1ZM8 7.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132Z"/>
             </svg></button>
             </div>
@@ -40,7 +40,7 @@ function cargarProductos() {
 cargarProductos()
 
 function activarClick(){
- const botonAgregar = document.querySelectorAll("button.boton-añadir")
+ const botonAgregar = document.querySelectorAll("button.boton-add")
   botonAgregar.forEach((boton) => {
          boton.addEventListener("click", (e) => {
             const id = parseInt(e.target.id)
